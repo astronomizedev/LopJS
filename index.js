358bae0115dedc6ce2bac9a78cb8f567e5e0ca54
astronomizedev <51801389+astronomizedev@users.noreply.github.com>
2019-08-27 21:55:30 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+var fs = require('fs'), path = require('path')
+
+function createGuiFromJSON(jsonFile) {
+    if(jsonFile.constructor === ({}).constructor) {
+        // takes up less space in the main file ^^
+        return require('./src/jsonUi.js').uiFromJson(jsonFile)
+    } else {
+        throw new TypeError('\'jsonFile\' parameter must be a json object.')
+    }
+}
+
+if(process.argv[2] != null) {
+    var file = process.argv[2]
+
+    var json = JSON.parse(fs.readFileSync(path.join((process.argv[1], file))))
+
+    fs.writeFileSync(file.replace('.json', '.html'), createGuiFromJSON(json))
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+# LopJS
+LopJS is a node application for developing beautiful websites with JSON(basically an interpreter)!
+
+![downloads](https://img.shields.io/npm/dt/lopjs)
