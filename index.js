595db21d834e4505f5352076237226b371a1540b
astronomizedev <51801389+astronomizedev@users.noreply.github.com>
2019-08-28 22:15:47 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 #!/usr/bin/env node
-var fs = require('fs'), path = require('path')
+var fs = require('fs'), path = require('path'), chalk = require('chalk')
 
 function createGuiFromJSON(jsonFile) {
     if(jsonFile.constructor === ({}).constructor) {
         // takes up less space in the main file ^^
-        return require('./src/jsonUi.js').uiFromJson(jsonFile)
+        return require('./src/nonMinimalized.js').uiFromJson(jsonFile)
     } else {
         throw new TypeError('\'jsonFile\' parameter must be a json object.')
     }
 }
 
 if(process.argv[2] != null) {
-    var file = process.argv[2]
+    
 
+    var file = process.argv[2]
+    console.log(chalk.green('Parsing json from \'' + file + '\''))
     var json = JSON.parse(fs.readFileSync(path.join((process.argv[1], file))))
 
     fs.writeFileSync(file.replace('.json', '.html'), createGuiFromJSON(json))
+    console.log(chalk.green('Saved to \'' + file.replace('.json', '.html') + '\''))
+
+    console.log(chalk.rgb(66, 245, 90)('Completed task successfully.'))
 }
